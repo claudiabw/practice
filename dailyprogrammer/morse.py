66a24b192ca16129251f5d6e7766db6675897490
Claudia <cwynn22@gmail.com>
2016-02-24 01:26:50 -0500

    add reverse dict

--- dailyprogrammer/morse.py
+++ dailyprogrammer/morse.py
@@ -1,14 +1,13 @@
 #daily programmer easy challenge 7
 
 morse_alphabet = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O':'---', 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..', '1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.', '0':'-----', ' ':'/'}
+reverse_alphabet = dict((v,k) for (k,v) in morse_alphabet.items())
 
 def encode(letter):
 	return morse_alphabet[letter]
 
 def decode(morse_letter):
-	for k, v in morse_alphabet.iteritems():
-		if v == morse_letter:
-			return k
+	return reverse_alphabet[morse_letter]
 
 string = raw_input("Enter a phrase to translate: ")
 
