bc586a4d9e032c07e38d121aa0be1b52debedba9
Claudia <cwynn22@gmail.com>
2016-02-27 19:32:56 -0500

    advent day 2

---
+++ advent-of-code/day2.py
@@ -0,0 +1,19 @@
+# advent of code day 2
+
+f = open('day2.txt', 'r')
+total = 0
+
+
+for line in f:
+	dimensions_array = line.split('\n')
+	dimensions = dimensions_array[0].split('x')
+	ints = []
+	for elem in dimensions:
+		ints.append(int(elem))
+	l = ints[0]
+	w = ints[1]
+	h = ints[2]
+	total += 2 * (l*w + w*h + l*h) + min(l*w, w*h, l*h)
+	print total
+
+print total
